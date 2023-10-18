9528af5f8d263985f63c2497aec5b9824385b4f0
Ch N V S S Durga Prasad <chodagiriprasad5@gmail.com>
2023-10-18 10:13:13 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 let msgContainer = document.getElementById("msgContainer");
 
 function sendInput(){
+    let msgBlock = document.createElement("div");
+    msgBlock.classList.add("msg-from-student")
     let paraEle = document.createElement("p");
     let inputValue = document.getElementById("inputValue");
-    paraEle.textContent = inputValue.value;
-    msgContainer.appendChild(paraEle);
-    inputValue.value = "";
+    if(inputValue.value!==""){
+        paraEle.textContent = inputValue.value;
+        paraEle.style.fontSize = "20px"
+        msgBlock.appendChild(paraEle);
+        inputValue.value = "";
+        msgContainer.appendChild(msgBlock);
+        let breakEl = document.createElement("br");
+        msgContainer.appendChild(breakEl);
+    }
 }
